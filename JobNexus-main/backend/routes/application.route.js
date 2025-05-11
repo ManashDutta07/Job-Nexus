e472bc8cc567eb7a57b0703361a8d82b7bb35742
Manash Dutta <91425602+ManashDutta07@users.noreply.github.com>
2025-05-11 11:48:02 +0530

    Update application.route.js

--- JobNexus-main/backend/routes/application.route.js
+++ JobNexus-main/backend/routes/application.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
  
-const router = express.Router();
+const router = express.Routers();
 
 router.route("/apply/:id").get(isAuthenticated, applyJob);
 router.route("/get").get(isAuthenticated, getAppliedJobs);
