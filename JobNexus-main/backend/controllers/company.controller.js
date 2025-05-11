06b0c78f4a1516f65c68f35c760be67addab0ba9
Manash Dutta <91425602+ManashDutta07@users.noreply.github.com>
2025-05-11 11:48:57 +0530

    Update company.controller.js

--- JobNexus-main/backend/controllers/company.controller.js
+++ JobNexus-main/backend/controllers/company.controller.js
@@ -6,7 +6,7 @@ export const registerCompany = async (req, res) => {
     try {
         const { companyName } = req.body;
         if (!companyName) {
-            return res.status(350).json({
+            return res.status(400).json({
                 message: "Company name is required.",
                 success: false
             });
